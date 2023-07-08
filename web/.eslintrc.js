237b55889b69546e870023f3354ae34e4eff554b
Konstantin <slipkid85@gmail.com>
2023-07-08 23:00:18 +0100

    new/update packages

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -5,16 +5,34 @@ module.exports = {
     },
     parser: "@typescript-eslint/parser",
     parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
         ecmaVersion: "latest",
         sourceType: "module",
     },
     plugins: ["@typescript-eslint"],
     root: true,
-    rules: {
-        "comma-dangle": ["warn", { objects: "always-multiline" }],
-        indent: ["warn", 4],
-        quotes: ["warn", "double"],
-    },
+    ignorePatterns: [
+        "webpack.config.js",
+        ".eslintrc.js"
+    ],
+    overrides: [
+        {
+            files: ["*ts", "*tsx"],
+            rules: {
+                indent: ["warn", 4],
+                quotes: ["warn", "double"],
+                "comma-dangle": ["warn", { objects: "always-multiline" }],
+                "@typescript-eslint/no-non-null-assertion": [ "off" ],
+                "no-use-before-define": [0, {
+                    functions: false,
+                    classes: false,
+                    variables: false,
+                    allowNamedExports: false,
+                }],
+            },
+        },
+    ],
     extends: [
         "semistandard",
         "eslint:recommended",
